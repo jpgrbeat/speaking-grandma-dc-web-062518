5ee905918c7ece884d2f4498217c8fa661ae8c1d
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-13 18:37:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -7,6 +7,18 @@
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
+def speak_to_grandma(person_says)
+  if person_says != person_says.upcase
+    'HUH?! SPEAK UP, SONNY!'
+  elsif person_says == "I LOVE YOU GRANDMA!"
+  "I LOVE YOU TOO PUMPKIN!"
+  else
+    "NO, NOT SINCE 1938!"
+    END
+END
+  
+
+
 # NO, NOT SINCE 1938!
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
