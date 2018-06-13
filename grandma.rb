18e1a6dc3cbc3295aba82ddd18627f75da7647f7
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-13 18:47:16 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -11,7 +11,7 @@ def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif string != string.upcase
-    "HUH?! SPEAK UP SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
   
